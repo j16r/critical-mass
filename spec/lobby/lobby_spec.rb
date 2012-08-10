de15ff0d961d5f4730f4f9b421bd729757e561e9
John Barker <jebarker@gmail.com>
2012-08-10 13:51:52 -0400

    Add rspec options and fix specs

--- spec/lobby/lobby_spec.rb
+++ spec/lobby/lobby_spec.rb
@@ -11,10 +11,9 @@ describe 'The lobby', :type => :request do
     context 'empty nickname' do
 
       it 'cannot login' do
-        puts page.body
         fill_in 'Nickname', :with => ''
-        click_on '#submit'
-        page.should have_css('.error')
+        click_on 'submit'
+        page.should have_css('#login')
       end
 
     end
@@ -25,7 +24,7 @@ describe 'The lobby', :type => :request do
 
       it 'can login' do
         fill_in 'Nickname', :with => player1_nickname
-        click_on '#submit'
+        click_on 'submit'
         page.should have_css('#currentUser', :text => player1_nickname)
       end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 
-Capybara.current_driver = :poltergeist
+Capybara.default_driver = :poltergeist
+Capybara.run_server = false
 Capybara.app_host = 'http://localhost:3000'
